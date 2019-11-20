0a0c34c0aa3d4abb018778dca5eb6a804065bb75
ashish <asah@calance.com>
2019-11-20 18:55:16 +0530

    Intial Change

---
+++ src/components/Header/header.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import  style from './header.css';
+
+
+const Header = ()=>{
+
+
+    return(
+      <header className={style.header}>
+       <div>Here</div>
+      </header>)
+
+}
+
+export default Header;
---
+++ src/hoc/layout/layout.js
@@ -0,0 +1,18 @@
+import React , {Component} from 'react';
+import Header from '../../components/Header/header';
+import Footer from '../../components/Footer/footer';
+import './layout.css';
+
+
+class Layout extends Component{
+
+   render(){
+    return(
+    <div>
+    <Header/>{this.props.children}<Footer/></div>
+    )
+   }
+
+}
+
+export default Layout;
